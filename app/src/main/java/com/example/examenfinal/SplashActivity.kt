eab037503e21d98a7c1a97dfbe54970d6edd904c
Karla R. Jimenez <48140862+KarlaaRJz@users.noreply.github.com>
2023-01-27 04:00:55 -0600

    SplashScreen Completo

--- app/src/main/java/com/example/examenfinal/SplashActivity.kt
+++ app/src/main/java/com/example/examenfinal/SplashActivity.kt
@@ -1,11 +1,21 @@
 package com.example.examenfinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.concurrent.thread
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        thread {
+            Thread.sleep(3000)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
